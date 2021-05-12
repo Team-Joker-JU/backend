29b52c76ddddec6934e8e954cfb97089da8b2cef
Supppee <hampusvenssonsupmah@outlook.com>
2021-05-12 13:16:54 +0200

    small changes to finalize the calls

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,12 @@
 import { Router } from 'itty-router';
 import Example from './resources/example';
-import { getCoordsBySession, postCoord } from './resources/coords';
+import { getCoordsByPage, postCoord } from './resources/coords';
 
 const router = Router();
 
 router
   .get('/api/example', Example)
-  .get('/api/coord/:session', getCoordsBySession)
+  .get('/api/coord/:page', getCoordsByPage)
   .post('/api/coord', postCoord)
   .get('*', () => new Response('Not found', { status: 404 }));
 
